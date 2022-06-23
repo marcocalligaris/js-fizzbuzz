176aa11f13e93e82f2cf21f68a493689764acc6f
marcocalligaris <marcocally@gmail.com>
2022-06-23 17:15:33 +0200

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -4,22 +4,29 @@
 
 // * 3 - Dico al programma di stampare Fizz, Buzz o Fizzbuzz a seconda del multiplo
 
+const list = document.getElementById('list');
 
+let listItem = '';
 
 for (let i = 1; i <= 100; i++) {
-
+  
   if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
+    listItem += 'FizzBuzz ';
 
   } else if (i % 3 === 0) {
     console.log('Fizz');
+    listItem += 'Fizz ';
 
   } else if (i % 5 === 0) {
     console.log('Buzz');
+    listItem += 'Buzz ';
 
   } else {
     console.log(i);
+    listItem += `${i} `;
   }
 
 }
 
+list.innerHTML = listItem;
